e0261c898c92362e8ea571031a18e234c33c1fe3
Penasta <toledis@gmail.com>
2021-10-18 13:51:17 -0300

    Plotando por região

--- r_docs/Mapas dos clusters.R
+++ r_docs/Mapas dos clusters.R
@@ -1,7 +1,7 @@
 # Agora, vamos plotar os clusters num mapa para entender melhor a visualização
 
 ## Pacotes necessários
-pacman::p_load(dplyr, stringr,foreign,tidyverse,ggplot2,factoextra,geobr,reshape2,sf,magrittr)
+pacman::p_load(dplyr, stringr,foreign,tidyverse,ggplot2,factoextra,geobr,reshape2,sf,magrittr,purrr)
 ##
 
 ##Trocando o nome para não confundir
@@ -23,7 +23,7 @@ head(metadata)
 #Agora sequestrar a coluna de geoplotagem
 all_mun <-read_municipality()
 all_mun <- all_mun %>%
-  select(code_muni,geom)
+  select(code_muni,geom,code)
 
 #Refazendo e ajustando o passo feito anteriormente para adequar ao nome do banco do {geobr}
 cluster1$code_muni <- nome
@@ -47,4 +47,55 @@ ggplot() +
 #Segundo mapa: Municípios com formação docente superior específica
 ggplot() +
   geom_sf(data=cluster2, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4)
+
+######
+#Teste de  plotagem por região
+#somente Cluster1; Formação genérica
+
+#Criando coluna com codigo da  uf (2 dígitos)
+cluster1$code_state <- str_sub(cluster1$code_muni,1,2)
+cluster1$code_state <- as.numeric(cluster1$code_state)
+
+#Sul
+sul <- cluster1 %>%
+  filter(code_state == "43" |code_state == "42" |code_state == "41")
+
+ggplot() +
+  geom_sf(data=sul, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4)
+
+#Sudeste
+sudeste <- cluster1 %>%
+  filter(code_state == "35" |code_state == "33" |code_state == "32" |code_state == "31")
+
+ggplot() +
+  geom_sf(data=sudeste, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4)
+
+#Centro-oeste
+
+centro <- cluster1 %>%
+  filter(code_state == "50" |code_state == "51" |code_state == "52" |code_state == "53")
+
+ggplot() +
+  geom_sf(data=centro, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4)
+
+#Nordeste
+nordeste <- cluster1 %>%
+  filter(code_state == "21" |code_state == "22" |code_state == "23" |code_state == "24" | code_state == "25" |code_state == "26" |
+           code_state == "27" |code_state == "28" |code_state == "29" )
+
+ggplot() +
+  geom_sf(data=nordeste, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4)
+
+#Norte
+norte <- cluster1 %>%
+  filter(code_state == "11" |code_state == "12" |code_state == "13" |code_state == "14" | code_state == "15" |code_state == "16" |
+           code_state == "17")
+
+ggplot() +
+  geom_sf(data=norte, aes(geometry=geom,fill=cluster), color= NA, size=.15)+
   scale_fill_viridis_c(trans = "sqrt", alpha = .4)
