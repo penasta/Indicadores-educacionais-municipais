516f31eb75da5f77c30ef36727d85fa465a55966
Penasta <toledis@gmail.com>
2021-11-07 19:11:03 -0300

    rs

--- r_docs/Cluster.R
+++ r_docs/Cluster.R
@@ -1,3 +1,16 @@
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### 
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### 
+
+######### AVISO
+######### SCRIPT INTENSAMENTE MODIFICADO AO LONGO DO SEMESTRE SEM REVISÃO GERAL, O QUE SIGNIFICA PRESENÇA DE DIVEERSAS LINHAS DE CÓDIGO MORTO
+######### RODAR O SCRIPT POR INTEIRO PROVAVELMENTE NÃO FUNCIONARÁ COMO SE ESPERA
+######### Autor do código: Bruno Gondim toledo; com colaborações de: João Doretto e Luiza Servo
+######### O script aqui presente é livre para uso, modificação, publicação e demais finalidades, sem necessidade de autorização ou crédito ao autor.
+######### Pela democratização do conhecimento. Fora Bolsonaro.
+
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### 
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### ######### 
+
 ## Pacotes necessários
 
 pacman::p_load(dplyr, stringr,foreign,tidyverse,ggplot2,factoextra,readxl,xlsx)
